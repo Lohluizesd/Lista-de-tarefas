c9cc79f5ed1e4e13a77b32bfc9119ae7ab096955
Lorenna Dias <lohluizesd@gmail.com>
2024-05-08 20:58:56 -0300

    feat: update menu mensage

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
-var adicionarTarefa = document.querySelector('.adicionar__botao');
+var btnAdicionarTarefa = document.querySelector('.adicionar__botao');
+var btnMostrarMenu = document.querySelector('.mostrar__menu');
+var formAdicionarTarefa = document.querySelector('.formulario__tarefas');
+
+
+/*Essa função vai mostrar o formulario quando vc clicar no '+'*/
+btnMostrarMenu.addEventListener('click', () =>{
+    formAdicionarTarefa.classList.toggle('hidden');
+})
+
+/*Botão que adiciona a tarefa*/
+
+
+/*Evento de submit*/
+
+/*Função que coloca a tarefa na lista de itens*/
+
+/*Lista de itens - JSON*/
+
 
