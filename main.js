9bf5bbcf3ed1c2d5a7cca7f95d476a5be9b84c6e
Lorenna Dias <lohluizesd@gmail.com>
2024-05-14 21:17:17 -0300

    Edição  final até então

--- main.js
+++ main.js
@@ -1,40 +1,48 @@
 var btnAdicionarTarefa = document.querySelector('.adicionar__botao');
 var btnMostrarMenu = document.querySelector('.mostrar__menu');
 var formAdicionarTarefa = document.querySelector('.formulario__tarefas');
-var textArea = document.querySelector('app__form-textarea');
-
+var textArea = document.querySelector('.app__form-textarea');
+var listaTarefas = document.querySelector('.lista__tarefas');
 
 /*Essa função vai mostrar o formulario quando vc clicar no '+'*/
 btnMostrarMenu.addEventListener('click', () =>{
     formAdicionarTarefa.classList.toggle('hidden');
-})
+});
+
+/*Função que coloca a tarefa na lista de itens*/
+function adicionarTarefa(tarefa){
+    var li = document.createElement('li');
+    li.classList.add('lista__tarefas__item');
+
+    var paragrafo = document.createElement('p');
+    paragrafo.textContent = tarefa.descricao;
+    paragrafo.classList.add('lista__tarefas__item');
+
+    li.appendChild(paragrafo);
+
+    return li;
+};
+
+/*Lista de itens - JSON*/
+var tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
+
+tarefas.forEach(tarefa => {
+    var elementoTarefa = adicionarTarefa(tarefa);
+    listaTarefas.appendChild(elementoTarefa);
+});
+
 
 /*Botão que adiciona a tarefa*/
-formAdicionarTarefa.addEventListener('submit', (evento) => {
-    evento.preventDefault();
+btnAdicionarTarefa.addEventListener('click', () => {
     var tarefa = {
         descricao: textArea.value
     }
     tarefas.push(tarefa);
+
+    var elementoTarefa = adicionarTarefa(tarefa);
+    listaTarefas.appendChild(elementoTarefa);
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+    textArea.value = '';
+    
 });
-
-/*Evento de submit*/
-
-/*Função que coloca a tarefa na lista de itens*/
-function adicionarTarefa(tarefa){
-    var li = document.querySelector('li');
-    li.classList.add('.lista__tarefas__item');
-
-    var paragrafo = document.createElement('p');
-    paragrafo.textContent = tarefa.descricao;
-    paragrafo.classLista.add('.lista__tarefas__item');
-
-    li.append(paragrafo);
-
-    return li;
-}
-/*Lista de itens - JSON*/
-var tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
-
-
 
